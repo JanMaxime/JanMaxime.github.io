aa607c5bed3ea9ab43a6b079d717f4459727f7f1
Jan Maxime <maxime.jan@epfl.ch>
2021-03-10 17:15:42 +0100

    Test Json

--- map.js
+++ map.js
@@ -43,8 +43,9 @@ var marker2 = L.marker([46.531, 6.63]).addTo(map).on('click', function(e) {
 });
 
 fetch("JanMaxime.github.io/data.json").then(function(data) {
-    console.log(data.length)
-    for (var i = 0; i < data.length; i++) {
+    console.log(data.json.length)
+    console.log(data.json);
+    for (var i = 0; i < data.json.length; i++) {
         console.log(data[i]);
     }
 });
